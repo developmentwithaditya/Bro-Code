9c231f9b5d3289c99fa98689a9f53597643d2c3a
developmentwithaditya <developmentwithaditya@gmail.com>
2024-05-17 17:02:47 +0530

    fucntions in c

---
+++ C Files/Function/function.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+void birthdaywish() {
+    printf("\nHappy birthday to you!");
+    printf("\nHappy birthday to you!");
+    printf("\nHappy birthday dear...YOU!");
+    printf("\nHappy birthday to you! \n");
+    // this is a function name birthdaywish that wish birthday
+}
+
+int main() {
+
+// Function is a small piece of code that that perfrom a task execute when we call it 
+// Its is usefull when you wnat to perfrom a task repeatly
+
+birthdaywish(); // we can invoke this fuction many time we want
+birthdaywish();
+
+    return 0;
+}
